27db4dfef6c6fd4c4af744f911db602658dbc9cd
tlrh314 <timohalbesma@gmai.com>
2014-03-16 23:28:26 +0100

    Fixed IvOAuth

--- CARD/registration/views.py
+++ CARD/registration/views.py
@@ -2,14 +2,27 @@
 Views which allow users to create and activate accounts.
 
 """
-
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render_to_response
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
+#from django.views import generic
+from django.http import HttpResponse, HttpResponseRedirect, \
+    HttpResponseBadRequest, HttpResponseNotFound
+from django.contrib.auth import authenticate,login
 
 from registration import signals
 from registration.forms import RegistrationForm
 
+import re
+import json
+import logging
+
+from urllib import urlencode
+from urllib2 import urlopen, HTTPError
+
+from CARD.settings import LOGIN_REDIRECT_URL, IVOAUTH_TOKEN, IVOAUTH_URL
+
+logger = logging.getLogger('registration')
 
 class _RequestPassingFormView(FormView):
     """
@@ -140,3 +153,109 @@ class ActivationView(TemplateView):
 
     def get_success_url(self, request, user):
         raise NotImplementedError
+
+
+def ivoauth(request):
+    callback_url = str(request.build_absolute_uri("ivoauth/callback")) + \
+        "/?ticket={#ticket}"
+    post_data = [('token', IVOAUTH_TOKEN), ('callback_url', callback_url)]
+    try:
+        content = json.loads(urlopen(IVOAUTH_URL + "/ticket",
+                             urlencode(post_data)).read())
+    except HTTPError:
+        logger.error("Invalid url")
+        return HttpResponseBadRequest()
+
+    if content["status"] == "success":
+        logger.debug("IVO authentication successful")
+        return HttpResponseRedirect(IVOAUTH_URL + "/login/" +
+                                    content["ticket"])
+    else:
+        logger.debug("IVO authentication failed")
+    return HttpResponseBadRequest()
+
+
+def ivoauth_callback(request):
+    ticket = request.GET.get("ticket", "")
+    if not ticket:
+        logger.error("no ticket")
+    url = IVOAUTH_URL + "/status"
+    post_data = [('token', IVOAUTH_TOKEN), ('ticket', ticket)]
+    try:
+        content = urlopen(url, urlencode(post_data)).read()
+    except HTTPError:
+        logger.error("Invalid url")
+        return HttpResponseBadRequest()
+
+    content = json.loads(content)
+    if content["status"] == "success":
+        logger.debug("Authentication successful")
+        attributes = content["attributes"]
+        external_id = "surfconext/" + attributes["saml:sp:NameID"]["Value"]
+        email = attributes["urn:mace:dir:attribute-def:mail"][0]
+#        person_set = Person.objects.filter(external_id=external_id)
+        # TODO what if a person with same name/email exists?
+        # 1. Ask for confirmation
+        # 2. Notify admin?
+        # 3. Send email
+#        if not person_set.exists():
+#            person = Person()
+#            person.UvANetID = attributes["urn:mace:dir:attribute-def:uid"][0]
+#            #surname = attributes["urn:mace:dir:attribute-def:sn"]
+#            first_name = attributes["urn:mace:dir:attribute-def:givenName"]
+#            person.name = attributes["urn:mace:dir:attribute-def:cn"][0]
+#            #displayname = attributes["urn:mace:dir:attribute-def:displayName"]
+#            person.email = email
+#            person.external_id = external_id
+#            logger.debug("Created new person '" + person.UvANetID + "'")
+#        else:
+#            person = person_set.get()
+#        if not person.user:
+#            user = User()
+#            user.username = person.UvANetID
+#            user.first_name = first_name
+#            user.email = email
+#            user.set_password(utils.id_generator(size=12))
+#            user.save()
+#            person.user = user
+#            logger.debug("User '{}' linked to person '{}'".
+#                         format(user, person))
+#        user = person.user
+#        person.save()
+#        user = authenticate(username=user.username)
+#        login(request, user)
+#        logger.debug("Logged in user '{}'".format(user))
+    else:
+        logger.debug("Authentication failed")
+    return HttpResponseRedirect('/')
+    #return render_to_response('registration/ivoauth_callback.html', attributes)
+    # Print content
+    # html = "<html><body>%s.</body></html>" % content
+    # return HttpResponse(html)
+
+#def login_user(request):
+#    username = password = redirect = ''
+#    state = 'Not logged in'
+#    if request.method == "POST" and request.is_ajax():
+#        username = request.POST['username']
+#        password = request.POST['password']
+#        redirect = request.POST.get('next', '/')
+#        user = authenticate(username=username, password=password)
+#        if user is not None and user.is_active:
+#            state = 'Logged in'
+#            login(request, user)
+#
+#            # Check if redirecturl valid
+#            if '//' in redirect and re.match(r'[^\?]*//', redirect):
+#                redirect = LOGIN_REDIRECT_URL
+#            data = json.dumps({'success': True,
+#                               'redirect': redirect})
+#        else:
+#            data = json.dumps({'success': False,
+#                               'redirect': redirect})
+#        return HttpResponse(data, mimetype='application/json')
+#    return HttpResponseBadRequest()
+#
+#def logout_user(request):
+#    logout(request)
+#    return HttpResponseRedirect('/')
