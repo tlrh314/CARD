2b8b94227dde209d64c86bc7ae1e64fedd6bc469
tlrh314 <timohalbesma@gmai.com>
2014-03-17 15:02:23 +0100

    Added external_id

--- CARD/registration/forms.py
+++ CARD/registration/forms.py
@@ -30,16 +30,18 @@ class RegistrationForm(forms.Form):
 
     required_css_class = 'required'
 
+    external_id = None
     #username = forms.RegexField(regex=r'^[\w.@+-]+$', max_length=30,\
     #        label=_("Username"), error_messages={'invalid':\
     #        _("This value may contain only letters, numbers and"+\
     #        " @/./+/-/_ characters.")})
     username = forms.RegexField(regex=r'^[0-9]{7,9}$', label=_("UvANetID"), \
             error_messages={'invalid':\
-            _("A UvANetID consists of zeven or nine digits.")})
+            _("A UvANetID consists of zeven to nine digits.")})
     first_name = forms.CharField(label=_("First Name"))
     last_name = forms.CharField(label=_("Last Name"))
-    external_id = forms.CharField(label=_("external id"), max_length=75)
+    external_id = forms.CharField(label=_("external id"), max_length=75, \
+            required=False)
     email = forms.EmailField(label=_("E-mail"))
     password1 = forms.CharField(widget=forms.PasswordInput,label=_("Password"))
     password2 = forms.CharField(widget=forms.PasswordInput,\
