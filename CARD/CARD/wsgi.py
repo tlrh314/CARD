339fe0f5d24578cecd9d90bf0586095d40bb7a6d
tlrh314 <timohalbesma@gmai.com>
2014-03-25 17:19:00 +0100

    Fixed wrong wsgi path

--- CARD/CARD/wsgi.py
+++ CARD/CARD/wsgi.py
@@ -10,10 +10,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 import sys
 
-root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..',
-    '..'))
-sys.path.insert(0,
-        os.path.abspath(os.path.join(root_path,'/opt/virtualenv/card/lib/python2.7/site-packages')))
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..','..'))
+sys.path.insert(0,os.path.abspath(os.path.join(root_path,'/opt/virtualenv/card/lib/python2.7/site-packages')))
 sys.path.insert(0, os.path.abspath(os.path.join(root_path,'card')))
 sys.path.insert(0, os.path.abspath(os.path.join(root_path,'card','CARD')))
 
