cdf08181f581522ba05d4461b7ecb97b15052b62
tlrh314 <timohalbesma@gmai.com>
2014-04-03 13:17:49 +0200

    Change ivoauth to accounts/ivoauth

--- CARD/CARD/urls.py
+++ CARD/CARD/urls.py
@@ -8,8 +8,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'ivoauth/callback', 'registration.views.ivoauth_callback'),
-    url(r'ivoauth/$', 'registration.views.ivoauth', name='ivoauth'),
-    url(r'^logout', 'registration.views.logout_user', name='logout'),
     url(r'^courses/', include('education.urls',namespace="education"))
 )
